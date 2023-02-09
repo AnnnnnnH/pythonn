2def10e6886790a16d1cbdcf97abc049909782f1
Yessine <104738552+AnnnnnnH@users.noreply.github.com>
2023-02-09 03:37:33 +0100

    Add files via upload

--- py/agency.py
+++ py/agency.py
@@ -1,44 +1,48 @@
 from datetime import datetime
+import numpy as np
 from voiture import Voiture
+
 class Agence:
     def __init__(self):
         self.voitures = []
 
-    def ajouter_voiture(self, voiture):
-     if self.rechercher_voiture_par_matricule(voiture.matricule):
-        print("La voiture avec la matricule " + voiture.matricule + " existe déjà.")
-        return
-     self.voitures.append(voiture)
+    # ... Other methods ...
 
-    def supprimer_voiture(self, matricule):
-        for i, v in enumerate(self.voitures):
-            if v.matricule == matricule:
-                del self.voitures[i]
-                return True
-        return False
+    def transformer_voiture(self):
+        self.voitures = [Voiture(*v) for v in self.voitures]
 
-    def rechercher_voiture_par_matricule(self, matricule):
-        for v in self.voitures:
-            if v.matricule == matricule:
-                print("voiture trouvez")
-                return True
-        print("voiture non trouvez")
-        return False
+    def calcul_distances(self, voiture):
+        features = np.array([(v.date_circulation - voiture.date_circulation).days,
+                             v.kilometrage,
+                             v.cylindrage] for v in self.voitures)
+        distances = np.linalg.norm(features - [voiture.date_circulation, voiture.kilometrage, voiture.cylindrage], axis=1)
+        return distances
 
-    def afficher_voitures(self):
-        for v in self.voitures:
+    def trier_voitures(self, distances):
+        self.voitures = [v for _, v in sorted(zip(distances, self.voitures), key=lambda x: x[0])]
+
+    def afficher_tri(self, indice):
+        for i, v in enumerate(self.voitures[:indice]):
             v.afficher_voiture()
 
-    def trier_selon_date_circulation(self):
-        self.voitures.sort(key=lambda x: x.date_circulation)
+    def rechercher_voitures_par_similarite(self):
+        voiture = Voiture()
+        voiture.saisir_voiture()
+        distances = self.calcul_distances(voiture)
+        self.trier_voitures(distances)
+        self.afficher_tri(1)
 
-    def get_voiture_plus_recente(self):
-        self.trier_selon_date_circulation()
-        print("La voiture la plus récente est:")
-        self.voitures[-1].afficher_voiture()
+if __name__ =="__main__" :
+    agency = Agence()
 
-    def get_voiture_plus_ancienne(self):
-        self.trier_selon_date_circulation()
-        print("La voiture la plus ancienne est:")
-        self.voitures[0].afficher_voiture()      
+    # Add some example cars to the list of cars
+    agency.voitures = [["m1", "toyotz", datetime.strptime("2001-01-01", "%Y-%m-%d"), 100000, 1000],
+                       ["m", "bmw", datetime.strptime("2022-01-01", "%Y-%m-%d"), 200000, 2000],
+                       ["m", "AUdi", datetime.strptime("2019-01-01", "%Y-%m-%d"), 300000, 3000]]
 
+    # Transform the list of cars into `Voiture` objects
+    agency.transformer_voiture()
+    # Search for cars similar to an input car
+    agency.rechercher_voitures_par_similarite()
+
+# 
